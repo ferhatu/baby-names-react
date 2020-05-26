b92f9c6e60765eeeb55e6ecd061c64adbf4c8f02
ferhatu <ferhat_2001@yahoo.com>
2020-05-26 23:43:28 +0100

    update

--- src/BabyNames.js
+++ src/BabyNames.js
@@ -3,6 +3,9 @@ import React from "react";
 function BabyNames(props) {
   return (
     <div className="container">
+      <div>
+        <input type="search" placeholder="Search Name" className="search" />
+      </div>
       <div className="names-container">
         {props.data.map((data) => {
           return (
