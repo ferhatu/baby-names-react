ec1df07112bd1758e85e5a1445b03be65d009705
ferhatu <ferhat_2001@yahoo.com>
2020-05-26 21:02:05 +0100

    update

--- src/BabyNames.js
+++ src/BabyNames.js
@@ -2,14 +2,16 @@ import React from "react";
 
 function BabyNames(props) {
   return (
-    <div className="names-container">
-      {props.data.map((data) => {
-        return (
-          <ul>
-            <li className="name">{data.name}</li>
-          </ul>
-        );
-      })}
+    <div className="container">
+      <div className="names-container">
+        {props.data.map((data) => {
+          return (
+            <div className="name">
+              <span className={data.sex}>{data.name}</span>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
