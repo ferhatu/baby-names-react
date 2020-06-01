3388f1d4a0967f23c25899764590e8ed664ea3a8
ferhatu <ferhat_2001@yahoo.com>
2020-06-01 19:45:06 +0100

    update

--- src/BabyNames.js
+++ src/BabyNames.js
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 function BabyNames(props) {
+  const [names, setNames] = useState(props.data);
+  const [inputValue, setInputValue] = useState("");
+
+  const filteredNames = () => {
+    setNames(
+      names.filter((baby) => baby.name.toLowerCase().includes(inputValue))
+    );
+  };
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value.toLowerCase());
+    filteredNames();
+  };
   return (
     <div className="container">
       <div>
-        <input type="search" placeholder="Search Name" className="search" />
+        <input
+          type="search"
+          value={inputValue}
+          onChange={handleChange}
+          placeholder="Search Name"
+          className="search"
+        />
       </div>
       <div className="names-container">
-        {props.data.map((data) => {
-          return (
-            <div className="name">
-              <span className={data.sex}>{data.name}</span>
-            </div>
-          );
-        })}
+        {names
+
+          .sort((a, b) => (a.name > b.name ? 1 : -1))
+          .map((data) => {
+            return (
+              <div className="name">
+                <span className={data.sex}>{data.name}</span>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
